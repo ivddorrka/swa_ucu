b8815db50833012f7f7cc7387c60373fbb323f13
ivddorrka <ivddorrka@gmail.com>
2023-04-03 22:10:25 +0200

    exec for curl

---
+++ crl_exec.sh
@@ -0,0 +1,12 @@
+curl -X POST -H "Content-Type: application/json" -d '{"message": "msg1"}' http://localhost:8080/
+curl -X POST -H "Content-Type: application/json" -d '{"message": "msg2"}' http://localhost:8080/
+curl -X POST -H "Content-Type: application/json" -d '{"message": "msg3"}' http://localhost:8080/
+curl -X POST -H "Content-Type: application/json" -d '{"message": "msg4"}' http://localhost:8080/
+curl -X POST -H "Content-Type: application/json" -d '{"message": "msg5"}' http://localhost:8080/
+curl -X POST -H "Content-Type: application/json" -d '{"message": "msg6"}' http://localhost:8080/
+curl -X POST -H "Content-Type: application/json" -d '{"message": "msg7"}' http://localhost:8080/
+curl -X POST -H "Content-Type: application/json" -d '{"message": "msg8"}' http://localhost:8080/
+curl -X POST -H "Content-Type: application/json" -d '{"message": "msg9"}' http://localhost:8080/
+curl -X POST -H "Content-Type: application/json" -d '{"message": "msg10"}' http://localhost:8080/
+
+curl http://localhost:8080
